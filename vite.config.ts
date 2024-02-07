c558f624b5daddf8453804bc4a4080a9a8d9c24b
aron <alherba1@outlook.com>
2024-02-07 17:57:35 -0500

    port update

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-
-	server: {
-		port: 3000, // Specify your desired port here
-	  },
-	plugins: [sveltekit()]
+  server: {
+    port: 3000, // Specify your desired port here
+  },
+  plugins: [sveltekit()]
 });
